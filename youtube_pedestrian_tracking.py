f58b46454f86be9238be59f2a9c177367cd63cf3
gregtam <gregtam1991@gmail.com>
2016-09-26 15:21:59 -0400

    Added different resolutions options (doesn't load properly however)

--- youtube_pedestrian_tracking.py
+++ youtube_pedestrian_tracking.py
@@ -11,38 +11,95 @@ Options:
 -t: sleep time (The amount of time to wait between displaying frames. This is 1/frame_rate. Default=33)
 -k: keep video (If tag is present, then the original YouTube video will not be deleted.)
 --save_as: If present, then it saves the video. This tag specifies output video name.
+--input_link: Unique youtube id code of video we wish to download.
 """
 
-from __future__ import unicode_literals
+from __future__ import unicode_literals  # used to properly download the youtube video
 from datetime import datetime
-import getopt
+import getopt  # module for switch options
 import os
+import subprocess  # used to access command line via Python
 import sys
 
 import cv2
+import pandas as pd
 import numpy as np
 import youtube_dl
 
 # Import command-line arguments
 args = sys.argv[1:]
-optlist, args = getopt.getopt(args, 'ks:t:', ['save_as=', 'input_link='])
+optlist, args = getopt.getopt(args, 'ks:t:', ['resolution=', 'save_as='])
 optdict = dict(optlist)
 
 save_video = '--save_as' in optdict
 if save_video:
     file_name = optdict['--save_as']
 
-if '--input_link' in optdict:
-    youtube_id = optdict['--input_link']
-    # ydl_opts = {'outtmpl': os.getcwd() + '/temp_youtube_vid.mp4'}
-    ydl_opts = {'outtmpl': '{path}/temp_youtube_vid_{id}.mp4'.format(path=os.getcwd(), id=youtube_id)}
-    if 'temp_youtube_vid.mp4' in os.listdir('.'):
+if len(args) == 1:
+    youtube_id = args[0]
+
+    choose_res = '--resolution' in optdict
+    if choose_res:
+        res = int(optdict['--resolution'])
+        # Get the youtube video's available resolutions.
+        # This works by sending a terminal command and retrieving the results
+        cmd = 'youtube-dl -F {id}'.format(id=youtube_id).split()
+        output = subprocess.Popen( cmd, stdout=subprocess.PIPE ).communicate()[0]
+        output = output.strip('\n')
+
+        # Split the output lines up
+        output_lines = np.array(output.split('\n'))
+        # Starting point of the resolution
+        res_index = int(np.where(output_lines == 'format code  extension  resolution note')[0])
+        # Select only lines corresponding to resolution information
+        output_lines = output_lines[res_index + 1:]
+
+        # Create a table with the resolution information
+        fmt_list = []
+        code_list = []
+        ext_list = []
+        for i in range(len(output_lines)):
+            temp_format, temp_code, temp_ext = output_lines[i].split()[:3]
+            fmt_list.append(temp_format)
+            code_list.append(temp_code)
+            ext_list.append(temp_ext)
+            
+        df = pd.DataFrame()
+        df['fmt'] = [int(i) for i in fmt_list]
+        df['code'] = code_list
+        df['ext'] = ext_list
+
+        # Exclude audio files
+        df = df[df.ext != 'audio'].reset_index(drop=True)
+        # Only include mp4 files
+        df = df[df.code == 'mp4'].reset_index(drop=True)
+        # Split the resolution into corresponding width and height
+        # columns, then convert to int
+        df['width'], df['height'] = zip(*df['ext'].map(lambda res: [int(i) for i in res.split('x')]))
+
+        if res in df['height'].tolist():
+            temp_df = df[df.height == res].reset_index(drop=True)  # select rows with the resolution
+            format_id = temp_df.fmt[0]
+        else:
+            raise Exception('This resolution is not available for this YouTube video.')
+
+        ydl_opts = {'outtmpl': '{path}/temp_youtube_vid_{id}.mp4'.format(path=os.getcwd(), id=youtube_id),
+                    'format': str(format_id)}
+    else:
+        ydl_opts = {'outtmpl': '{path}/temp_youtube_vid_{id}.mp4'.format(path=os.getcwd(), id=youtube_id)}
+
+    # If file already exists, remove it
+    if 'temp_youtube_vid_{id}.mp4'.format(id=youtube_id) in os.listdir('.'):
         os.remove('temp_youtube_vid_{id}.mp4'.format(id=youtube_id))
+
+    # Download the YouTube video locally
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download(['https://www.youtube.com/watch?v={id}'.format(id=youtube_id)])
+elif len(args) == 0:
+    raise Exception('Must specify a YouTube ID')
 else:
-    raise Exception('Must specify Youtube link as input link')
-# del unicode_literals
+    raise Exception('Must specify only one YouTube ID')
+
 
 start = datetime.now()
 cap = cv2.VideoCapture('temp_youtube_vid_{id}.mp4'.format(id=youtube_id))
